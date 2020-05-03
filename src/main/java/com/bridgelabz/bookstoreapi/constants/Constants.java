31b16248d3a3b4dc368e06c150d1e21a0fe4f44a
saikiranmsk <msaikiran0728@gmail.com>
2020-05-03 22:22:42 +0530

    [Sai] refractor. changed the user to users

--- src/main/java/com/bridgelabz/bookstoreapi/constants/Constants.java
+++ src/main/java/com/bridgelabz/bookstoreapi/constants/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	
 	public static final String REGISTRATION_STATUS = "Registration Confirmation";
 	public static final String REGISTRATION_MESSAGE = ", you have successfully Registrered to our website\nPlease click on below link to verify:\n";
-	public static final String VERIFICATION_LINK = "http://localhost:8081/users/registration/verify/";
+	public static final String VERIFICATION_LINK = "http://localhost:8080/users/registration/verify/";
 	public static final String RESET_MSG = "Click On the below link to reset your password";
 	public static final String RESET_PASSWORD_LINK = "Using below link reset your password\nhttp://localhost:3000/resetpassword/";
 	
