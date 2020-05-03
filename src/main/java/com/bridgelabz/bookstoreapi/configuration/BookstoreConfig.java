4879d1e5b1657e978efdbb435957fba695d28ca0
iamshaiksha <iamshaiksha2019@gmail.com>
2020-05-03 22:14:32 +0530

    [shaiksha]add. sellerController,sellerService,swagger,sellerresponse,sellerserviceimpl

--- src/main/java/com/bridgelabz/bookstoreapi/configuration/BookstoreConfig.java
+++ src/main/java/com/bridgelabz/bookstoreapi/configuration/BookstoreConfig.java
@@ -9,7 +9,6 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.bridgelabz.bookstoreapi.constants.Constants;
 
@@ -17,7 +16,7 @@ import com.bridgelabz.bookstoreapi.constants.Constants;
 public class BookstoreConfig {
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
--- src/main/java/com/bridgelabz/bookstoreapi/service/impl/SellerServiceImpl.java
+++ src/main/java/com/bridgelabz/bookstoreapi/service/impl/SellerServiceImpl.java
@@ -1,13 +1,12 @@
 package com.bridgelabz.bookstoreapi.service.impl;
 
-import java.util.regex.Pattern;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.bridgelabz.bookstoreapi.configuration.Consumer;
 import com.bridgelabz.bookstoreapi.configuration.Producer;
@@ -18,7 +17,6 @@ import com.bridgelabz.bookstoreapi.dto.RegisterDto;
 import com.bridgelabz.bookstoreapi.entity.Seller;
 import com.bridgelabz.bookstoreapi.exception.SellerException;
 import com.bridgelabz.bookstoreapi.repository.SellerRepository;
-import com.bridgelabz.bookstoreapi.response.Response;
 import com.bridgelabz.bookstoreapi.service.SellerService;
 import com.bridgelabz.bookstoreapi.utility.JWTUtil;
 import com.bridgelabz.bookstoreapi.utility.Token;
@@ -31,11 +29,11 @@ public class SellerServiceImpl implements SellerService{
 	private SellerRepository sellerRepository;
 	
 	@Autowired
-	private PasswordEncoder encoder;
+	private BCryptPasswordEncoder encoder;
 
 	@Autowired
 	private Producer producer;
-
+	
 	@Autowired
 	private Consumer consumer;
 	
@@ -47,19 +45,26 @@ public class SellerServiceImpl implements SellerService{
 	
 	/**
 	 * Saves the user details
+	 * @param register
+	 * @Return 
 	 */
+	@Transactional
 	@Override
-	public void sellerRegistration(RegisterDto register) {
+	public boolean sellerRegistration(RegisterDto register) {
 
 		if (sellerRepository.findByEmailAddress(register.getEmailAddress()).isPresent())
 			//throw new SellerException(HttpStatus.FOUND.value(), env.getProperty("103"));
 
 		register.setPassword(encoder.encode(register.getPassword()));
+		System.out.println("password--->"+register.getPassword());
 		Seller seller = new Seller(register);
 		Mail mail = new Mail();
 		try {
-			Seller usr = sellerRepository.save(seller);
-			if (usr != null) {
+			String password=encoder.encode(register.getPassword());
+			seller.setPassword(password);
+			seller.setVerificationStatus(true);
+			Seller sellr = sellerRepository.save(seller);
+			if (sellr != null) {
 				mail.setTo(seller.getEmailAddress());
 				mail.setSubject(Constants.REGISTRATION_STATUS);
 				mail.setContext("Hi " + seller.getSellerName() + " " + Constants.REGISTRATION_MESSAGE
@@ -70,69 +75,36 @@ public class SellerServiceImpl implements SellerService{
 		} catch (SellerException e) {
 			throw new SellerException(400, env.getProperty("102"));
 		}
+		return false;
 	}
 
 
-//	/**
-//	 * Update the user verification status if the received token is valid
-//	 * @return integer value that is number of record that had been updated
-//	 */
-//	@Override
-//	public int updateVerificationStatus(String token) {
-//		Long id = jwt.decodeToken(token);
-//		try {
-//			return repository.updateUserVerificationStatus(id, LocalDateTime.now());
-//		} catch (UserException e) {
-//			throw new UserException(400, env.getProperty("102"));
-//		}
-//	}
-
+	
 	/**
 	 * Login to the application using login credential
 	 * @return user details which is necessary
+	 * @param login
+	 * @return token
 	 */
+	@Transactional
 	@Override
-	public Response loginByEmailOrMobile(LoginDTO login) {
+	public String loginByEmailOrMobile(LoginDTO login) {
 		
-		Seller seller = null;
-		boolean email = Pattern.compile("^((\"[\\w-\\s]+\")|([\\w-]+(?:\\.[\\w-]+)*)|(\"[\\w-\\s]+\")([\\w-]+(?:\\.[\\w-]+)*))(@((?:[\\w-]+\\.)*\\w[\\w-]{0,66})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$)|(@\\[?((25[0-5]\\.|2[0-4][0-9]\\.|1[0-9]{2}\\.|[0-9]{1,2}\\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\\]?$)").matcher(login.getMailOrMobile()).matches();
-		boolean mobile = Pattern.compile("^[0-9]{10}$").matcher(login.getMailOrMobile()).matches();
-		Long mbl = mobile ? Long.parseLong(login.getMailOrMobile()) : 0; 
-		seller = email ? sellerRepository.findByEmailAddress(login.getMailOrMobile()).orElseThrow(() -> new SellerException(404, env.getProperty("104"))) :
-			   mobile ? sellerRepository.findByMobile(mbl).orElseThrow(() -> new SellerException(404, env.getProperty("104"))) : null;
-
-		if (seller.isVerificationStatus() && seller !=null) {
-			if (encoder.matches(login.getPassword(), seller.getPassword())) {
-				return new Response(HttpStatus.OK.value(), env.getProperty("202"), 
-						jwt.generateToken(seller.getSellerId(),Token.WITHOUT_EXPIRE_TIME));
+		
+		 Seller seller = sellerRepository.getUser(login.getMailOrMobile());
+		 System.out.println("password--->"+seller.getPassword());
+//		 String password=encoder.encode(login.getPassword());
+//		 System.out.println("password--->"+password);
+		if (seller != null) {
+			if ( (encoder.matches(login.getPassword(), seller.getPassword()))) {
+				String token=jwt.generateToken(seller.getSellerId(), Token.WITH_EXPIRE_TIME);
+				System.out.println(token);
+				return token;
 			}
-			throw new SellerException(401, env.getProperty("401"));
 		}
 		return null;
-	}
-
-//	/**
-//	 * Send the jwt token to the user mail
-//	 */
-//	@Override
-//	public void sendTokentoMail(String emailAddress) {
-//
-//		User user = repository.findByEmailAddress(emailAddress)
-//				.orElseThrow(() -> new UserException(404, env.getProperty("104")));
-//		Mail mail = new Mail();
-//		mail.setTo(emailAddress);
-//		mail.setSubject(Constants.RESET_MSG);
-//		mail.setContext(Constants.RESET_PASSWORD_LINK + jwt.generateToken(user.getUserId()));
-//		producer.sendToQueue(mail);
-//		consumer.receiveMail(mail);
-//	}
-
-//	/**
-//	 * Provides the link to reset the password to user mail address
-//	 */
-//	@Override
-//	public int resetPassword(String token, String newPassword) {
-//		Long id = jwt.decodeToken(token);
-//		return repository.updatePassword(id, encoder.encode(newPassword), LocalDateTime.now());
-//	}
+		
+		
+ 
+	}	 
 }
