47b002a9fc6c5d55357904dc4df3091fdccd6192
iamshaiksha <iamshaiksha2019@gmail.com>
2020-05-04 11:08:35 +0530

    [shaiksha]add.forgetpassword,reset password api

--- src/main/java/com/bridgelabz/bookstoreapi/service/SellerService.java
+++ src/main/java/com/bridgelabz/bookstoreapi/service/SellerService.java
@@ -1,11 +1,38 @@
 package com.bridgelabz.bookstoreapi.service;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import com.bridgelabz.bookstoreapi.dto.LoginDTO;
 import com.bridgelabz.bookstoreapi.dto.RegisterDto;
+import com.bridgelabz.bookstoreapi.dto.sellerForgetPasswordDto;
+import com.bridgelabz.bookstoreapi.entity.Seller;
 import com.bridgelabz.bookstoreapi.response.Response;
 
 public interface SellerService {
-
+	/**
+	 * @param register
+	 * @return
+	 */
 	public boolean sellerRegistration(RegisterDto register);
+	/**
+	 * 
+	 * @param login
+	 * @return token
+	 */
 	public String loginByEmailOrMobile(LoginDTO login);
+	/**
+	 * 
+	 * @param emailAddress
+	 * @return
+	 */
+	public String forgotpassword(@Valid String emailAddress);
+	/**
+	 * 
+	 * @param token
+	 * @param forgetPasswordDto
+	 * @return
+	 */
+	public String resetpassword(@Valid String token, sellerForgetPasswordDto forgetPasswordDto);
 }
