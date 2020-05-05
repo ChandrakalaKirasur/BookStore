aa49d1aa01e99c8d7c30e90eeb35b590b4cdf773
iamshaiksha <iamshaiksha2019@gmail.com>
2020-05-05 11:18:58 +0530

    [shaiksha]add.changes done in seller part

--- src/main/java/com/bridgelabz/bookstoreapi/service/SellerService.java
+++ src/main/java/com/bridgelabz/bookstoreapi/service/SellerService.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.bookstoreapi.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,7 +8,6 @@ import com.bridgelabz.bookstoreapi.dto.LoginDTO;
 import com.bridgelabz.bookstoreapi.dto.RegisterDto;
 import com.bridgelabz.bookstoreapi.dto.sellerForgetPasswordDto;
 import com.bridgelabz.bookstoreapi.entity.Seller;
-import com.bridgelabz.bookstoreapi.response.Response;
 
 public interface SellerService {
 	/**
