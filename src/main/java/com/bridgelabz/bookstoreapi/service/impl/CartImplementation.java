3ea8605fecb2069b40d3e19cdc9ca553f836ee9f
saikiranmsk <msaikiran0728@gmail.com>
2020-05-06 23:08:09 +0530

    [Sai] refractor. deleted the cartRepository

--- src/main/java/com/bridgelabz/bookstoreapi/service/impl/CartImplementation.java
+++ src/main/java/com/bridgelabz/bookstoreapi/service/impl/CartImplementation.java
@@ -4,22 +4,17 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
 import com.bridgelabz.bookstoreapi.entity.Book;
 import com.bridgelabz.bookstoreapi.entity.CartDetails;
 import com.bridgelabz.bookstoreapi.entity.QuantityOfBooks;
 import com.bridgelabz.bookstoreapi.entity.User;
 import com.bridgelabz.bookstoreapi.exception.UserException;
 import com.bridgelabz.bookstoreapi.repository.BookRepository;
-import com.bridgelabz.bookstoreapi.repository.CartRepository;
 import com.bridgelabz.bookstoreapi.repository.UserRepository;
 import com.bridgelabz.bookstoreapi.service.CartService;
 import com.bridgelabz.bookstoreapi.utility.JWTUtil;
@@ -37,8 +32,6 @@ public class CartImplementation implements CartService{
 	@Autowired
 	private BookRepository bookRepository;
 	
-	@Autowired
-	private CartRepository cartRepository;
 	
 	
 	@Autowired
