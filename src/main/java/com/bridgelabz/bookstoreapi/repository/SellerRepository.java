7fbfa0966745d63cd02045fd093371c994038c9b
iamshaiksha <iamshaiksha2019@gmail.com>
2020-05-28 11:25:54 +0530

    [shaiksha]add.refractor adone according to frontend

--- src/main/java/com/bridgelabz/bookstoreapi/repository/SellerRepository.java
+++ src/main/java/com/bridgelabz/bookstoreapi/repository/SellerRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.bridgelabz.bookstoreapi.entity.Book;
 import com.bridgelabz.bookstoreapi.entity.Seller;
 
 public interface SellerRepository extends JpaRepository<Seller, Long>{
@@ -16,7 +18,8 @@ public interface SellerRepository extends JpaRepository<Seller, Long>{
 	Seller getUser(String email);
 	@Query(value=" select * from seller",nativeQuery=true)
 	List<Seller> getSellers();
-	@Query(value=" select * from seller where seller_id=?",nativeQuery=true)
+	@Query(value=" select * from seller where seller_id=? ",nativeQuery=true)
 	Seller findSellerById(Long id);
+	
 	
 }
